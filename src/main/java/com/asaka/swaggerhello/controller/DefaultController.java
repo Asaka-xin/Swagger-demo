b828bca2972d57806f5ed74ce5ced280de98430b
Asaka <Longzhu.Qianxun@qq.com>
2023-02-16 00:58:36 +0800

    Program for demo.

---
+++ src/main/java/com/asaka/swaggerhello/controller/DefaultController.java
@@ -0,0 +1,23 @@
+package com.asaka.swaggerhello.controller;/*
+ *@Author Asaka
+ *@Date 2023/2/16 0:29
+ */
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "默认分类")
+@RestController
+@RequestMapping("/default")
+public class DefaultController {
+
+    @Operation(summary = "默认信息")
+    @GetMapping("/hello")
+    public String index(){
+        return "hello world!";
+    }
+}
---
+++ src/test/java/com/asaka/swaggerhello/SwaggerHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asaka.swaggerhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
