a8efb1f399e879efbc2396914abf6816739fc9bb
Asaka <Longzhu.Qianxun@qq.com>
2023-02-16 23:09:12 +0800

    Update: Annotation for controller

--- src/main/java/com/asaka/swaggerhello/controller/DefaultController.java
+++ src/main/java/com/asaka/swaggerhello/controller/DefaultController.java
@@ -1,10 +1,12 @@
-package com.asaka.swaggerhello.controller;/*
+package com.asaka.swaggerhello.controller;
+/*
  *@Author Asaka
  *@Date 2023/2/16 0:29
  */
 
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
     @Operation(summary = "默认信息")
+    @Parameter(name = "id" , required = false, description = "序号")
     @GetMapping("/hello")
     public String index(){
-        return "hello world!";
+        return "Hello Swagger3!";
     }
 }
